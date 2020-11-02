22528d681c8e97cc1e6e3c05b07fa08fa2305e45
Anoop C H <droid.anoop@gmail.com>
2020-11-02 16:21:44 +0530

    add code documentation

--- app/src/main/java/ch/anoop/g_fresh/view/custom/BounceInterpolator.kt
+++ app/src/main/java/ch/anoop/g_fresh/view/custom/BounceInterpolator.kt
@@ -8,7 +8,7 @@ import kotlin.math.pow
  * Animation class for the Fav button animation effect.
  *      -- Combined with R.anim.fav_view_bounce for smoothness
  */
-class MyBounceInterpolator(private val amp: Double, private val freq: Double) : Interpolator {
+class BounceInterpolator(private val amp: Double, private val freq: Double) : Interpolator {
 
     override fun getInterpolation(time: Float) =
         (-1 * Math.E.pow(-time / amp) * cos(freq * time) + 1).toFloat()
